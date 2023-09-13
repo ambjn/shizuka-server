710a5a7e1b00c8d995b396362758c902f9ddf12b
ambjn <jainanuamber@gmail.com>
2023-09-13 11:25:14 +0530

    shizuka-server

---
+++ app/db/mongodb.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  mongoose.connection.on("connected", () =>
+    console.log("Connected to database successfully!")
+  );
+  mongoose.connection.on("error", (error) => console.error(error));
+  mongoose.connection.on("disconnected", () =>
+    console.log("Database is disconneted!")
+  );
+  mongoose.set("strictQuery", true);
+  mongoose.connect(process.env.MONGDB_URI, {});
+};
